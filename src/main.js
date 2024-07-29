ffaee0de65acbcbdc60c543ac29398c72a01c7e1
Adham <ahelal@users.noreply.github.com>
2024-07-29 21:02:28 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+  try {
+    // Get inputs
+    const openaiEndpoint = core.getInput('az-openai-endpoint', {
+      required: true
+    })
+    const openaiKey = core.getInput('az-openai-key', { required: true })
+    const ghToken = core.getInput('gh-token', { required: false })
+    const userContext = core.getInput('user-context', { required: false })
+
+    const runId = github.context.runId
+
+    // Output the payload for debugging
+    core.info(`The run ID ${runId}}`)
+
+    // core.info(`The context : ${JSON.stringify(github.context, null, 2)}`)
+  } catch (error) {
+    // Fail the workflow step if an error occurs
+    core.setFailed(error.message)
+  }
+}
+
+module.exports = {
+  run
+}
