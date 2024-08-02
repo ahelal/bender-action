912630d1b861bf34bf6d440955d6be7b2d0cfd06
Adham <ahelal@users.noreply.github.com>
2024-08-02 11:18:01 +0200

    new prompt

--- src/openai.js
+++ src/openai.js
@@ -8,16 +8,16 @@ function setupInitialMessage(context, jobLog) {
       role: 'system',
       content: `As a support software engineer assistant, your purpose is to identify errors and suggest solutions to fix them. 
 You'll receive GitHub Action job log that indicate failures. Your response should be formatted as text and follow these guidelines:
-1. If the information provided is sufficient:
+1. Sufficient Information Provided::
     - State the cause of the job failure.
     - Provide a solution to fix the error.
-2. If the information is insufficient or you're unable to suggest a solution, follow these guidelines:
+2. Insufficient Information or Unable to Suggest a Solution:
     - If there's a stacktrace or an error pointing to a specific file, request the content of that file with a single-line reply: 'CONTENT_OF_FILE_NEEDED "<valid unix path>"' (e.g., 'CONTENT_OF_FILE_NEEDED "src/index.js"'). if directory structure is provided you can cross reference the file path with the directory structure.
     - If there's no way forward, reply with 'Not enough information to provide a solution.'`
     }
   ]
-  // Message setup
 
+  // Message setup
   let userMessage = `Github Action log that failed\n--------\n${jobLog}\n`
 
   if (context.jobContext)
@@ -32,6 +32,7 @@ You'll receive GitHub Action job log that indicate failures. Your response shoul
   core.info(
     `Job definition context: '${context.jobContext.length > 0}' Dir context: '${context.dirContext.length > 0}' User context: '${context.userContext.length > 0}'`
   )
+
   return systemMessage.concat({ role: 'user', content: userMessage })
 }
 
---
+++ tests/api.test.js
@@ -0,0 +1,14 @@
+
+// const core = require('@actions/core')
+// const github = require('@actions/github')
+const getJob = require('../src/api')
+
+// function interpolate(target, context) {
+//     if (typeof target === 'string' || target instanceof String) {
+//       return interpolateStr(target, context)
+//     }
+//     return interpolateObj(target, context)
+//   }
+
+
+    
