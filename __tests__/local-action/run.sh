bff281545c20462afaf2ff52f0eb00860611e329
Adham <ahelal@users.noreply.github.com>
2024-08-05 12:47:03 +0000

    run fmt

--- __tests__/local-action/run.sh
+++ __tests__/local-action/run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd)"
-ROOT_DIR="$( cd "${SCRIPT_DIR}"/../../ && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOT_DIR="$(cd "${SCRIPT_DIR}"/../../ && pwd)"
 # TEST_DIR="$( cd "${ROOT_DIR}/__tests__/" && pwd)"
-LOCAL_ACTION_DIR="$( cd "${ROOT_DIR}/__tests__/local-action" && pwd)"
+LOCAL_ACTION_DIR="$(cd "${ROOT_DIR}/__tests__/local-action" && pwd)"
 
 # if global npm package not installed, install it
-if ! command -v local-action &> /dev/null
-then
-  npm install -g @actionsflow/local-action
+if ! command -v local-action &>/dev/null; then
+	npm install -g @actionsflow/local-action
 fi
 
 local-action run "${ROOT_DIR}" src/index.ts "${LOCAL_ACTION_DIR}/.env"
