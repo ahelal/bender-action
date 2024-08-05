93b68274863043c873484a53e81a6f10e765ba29
Adham <ahelal@users.noreply.github.com>
2024-08-05 12:39:38 +0000

    fixes to shell

--- __tests__/local-action/run.sh
+++ __tests__/local-action/run.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd)"
 ROOT_DIR="$( cd "${SCRIPT_DIR}"/../../ && pwd)"
-TEST_DIR="$( cd "${ROOT_DIR}/__tests__/" && pwd)"
+# TEST_DIR="$( cd "${ROOT_DIR}/__tests__/" && pwd)"
 LOCAL_ACTION_DIR="$( cd "${ROOT_DIR}/__tests__/local-action" && pwd)"
 
 # if global npm package not installed, install it
@@ -12,4 +12,4 @@ then
   npm install -g @actionsflow/local-action
 fi
 
-local-action run ${ROOT_DIR} src/index.ts ${LOCAL_ACTION_DIR}/.env
+local-action run "${ROOT_DIR}" src/index.ts "${LOCAL_ACTION_DIR}/.env"
